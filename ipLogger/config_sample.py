1f26a69381445685d218ee233ebd4ff05af7004e
Olmo Mezger <olmo@green-and-energy.com>
2016-04-04 08:10:18 -0600

    added sample config file.

---
+++ ipLogger/config_sample.py
@@ -0,0 +1,10 @@
+# config file. put here your parse.com keys and the objectId of the gateway
+# make a copy of this file in your repository and rename it as config.py. this file is ignored on git and wont be published. 
+
+def config():
+   C = {
+     'APPLICATION_ID': '',
+     'REST_API_KEY' : '',
+     'GATEWAY_PARSE_ID': ''
+   }
+   return C
