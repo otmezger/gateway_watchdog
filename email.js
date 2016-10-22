ff6a6ab4907c11bd0848b2a32d0403ad3108d5c0
Serge <sergiotec13@gmail.com>
2016-10-22 14:56:38 -0600

    Update email.js

--- email.js
+++ email.js
@@ -1,14 +1,14 @@
-//Este programa envía un correo cuando hay alarma por temperatura excesiva
-//Se puede ampliar para enviar alarma en las 5 variables
+//This program sends an email when there's an alarm due to excesive core temperatures
+//It can be expanded so it sends alarms for all 5 variables that are being measured in the RaspberryPi
 
-var sender = require('sendgrid-sender'); //biblioteca para enviar correos
+var sender = require('sendgrid-sender'); //library to send emails
 var json = require('json-file');
 
-var alarma_activa=0; //auxiliar para evitar enviar múltiples correos
+var alarma_activa=0; //auxiliar variable to avoid sending mutiple emails for the same reason
 
 
 function enviarEmail(){
-  var file = json.read('./package.json'); //Paquete para intercambio de datos
+  var file = json.read('./package.json'); //Package for data exchange
   var Voltage=file.get('Voltage');  
   var MemoryUse=file.get('MemoryUse');
   var Frequency=file.get('Frequency');
@@ -16,12 +16,12 @@ function enviarEmail(){
   var Temperature=file.get('Temperature');
   var Fecha=file.get('Fecha');
   var Hora=file.get('Hora');
-  if (Temperature>50 && alarma_activa=0){ //Enviar correo solo en caso de alarma, solo la primera vez
+  if (Temperature>50 && alarma_activa=0){ //Send an email in the case of alarm, just the first time
   sender('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', {  
   from: 'sajibu3108@gmail.com',
   to: 'stevenjb.wolf@gmail.com',
   subject: 'test message',
-  html: '<h1><font color="red">ALERTA ALTA TEMPERATURA</font></h1>'+ //Indica alarma correspondiente
+  html: '<h1><font color="red">ALERTA ALTA TEMPERATURA</font></h1>'+ //Indicates the corresponding alarm
 	'<h3>Voltaje es : '+ Voltage + 'V</h3>'+
 	'<h3>Uso de memoria es : '+ MemoryUse + '</h3>'+
 	'<h3>La frecuencia es : '+ Frequency + '</h3>'+
@@ -32,16 +32,16 @@ function enviarEmail(){
   if (err) {
     console.log('error while sending message: ' + err);
   } else {
-    console.log('message sent'); //Para debug
+    console.log('message sent'); //For debugging purposes
   }
 })
-alarma_activa=1;}//Avisa que ya no se deben enviar más correos
-else if (Temperature<=50){ //Si la situación vuelve a la normalidad, se resetea la condición de alarma
+alarma_activa=1;}//Won't send more emails
+else if (Temperature<=50){ //Resets alarm condition
 alarma_activa=0;} 
 ;}
 
 setTimeout(enviarEmail,4000); 
-setInterval(enviarEmail,30000); //Comprobar si existe alarma cada 30 s
+setInterval(enviarEmail,30000); //Checks for alarms every 30s
 
 module.exports=enviarEmail;
 
