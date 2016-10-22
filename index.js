797eda852f43425559cb4dd1edbd0e5b21e64af8
danb1704 <dnb1704@gmail.com>
2016-10-22 11:32:24 -0600

    Main app for watchdog
    Runs perro.js and email.js at same time

---
+++ index.js
@@ -0,0 +1,9 @@
+//Este programa integra la lectura de datos y el envío de correos
+
+var enviarEmail= require('./email');
+var perro = require('./perro');
+
+enviarEmail();
+perro();
+
+//node ./index.js
